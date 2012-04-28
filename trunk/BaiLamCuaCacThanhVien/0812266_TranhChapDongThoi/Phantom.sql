162e1bba25865e5be2c22cb92ce60bf8df5c84aa
hoangliem236@gmail.com <hoangliem236@gmail.com@1697131c-28fd-8d6f-480c-085ca7451a2a>
2012-04-28 06:49:34 +0000

    Nộp trước bài tranh chấp đồng thời, do hôm nay về quê nghĩ lễ tới 1-5 mới lên, có gì mọi người gưi qua mail nhé, sẽ còn review.

---
+++ trunk/BaiLamCuaCacThanhVien/0812266_TranhChapDongThoi/Phantom.sql
@@ -0,0 +1,24 @@
+
+--Transaction 1
+
+create procedure TestCase04_T1
+as
+begin
+	begin tran
+	set transaction isolation level repeatable read
+	select * from DE_SINHVIEN
+	waitfor delay '00:00:10'
+	select * from DE_SINHVIEN
+	commit tran
+end
+
+--Transaction 2
+
+create procedure TestCase04_T2
+as
+begin
+	begin tran
+	insert into DE_SINHVIEN values(6,'0812266')
+	commit tran
+end
+
