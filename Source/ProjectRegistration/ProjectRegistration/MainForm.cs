406b2df6befd1c6783e3444eb9763f886862cce5
7hanatos13@gmail.com <7hanatos13@gmail.com@1697131c-28fd-8d6f-480c-085ca7451a2a>
2012-05-13 13:22:47 +0000

    Tạo giao diện chính và form About.

---
+++ Source/ProjectRegistration/ProjectRegistration/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectRegistration
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangKyDoAn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCapNhatDoAn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHuyDoAn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCapNhatSLSV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            About form = new About();
+            form.Show();
+
+        }
+    }
+}
