f90e675b194a62413b28e1bc82ce25899bc2a99f
7hanatos13@gmail.com <7hanatos13@gmail.com@1697131c-28fd-8d6f-480c-085ca7451a2a>
2012-06-01 12:56:57 +0000

    Xóa các file và thư mục cũ trong Document. Tổng hợp các tài liệu thành 1 file duy nhất. Xóa 2 thư mục Phantom và Lost Update từ phân công lần 2.

--- BaiLamCuaCacThanhVien/Lost Update/Error/LostUpdate_T1.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRAN
-DECLARE @sldk INT
-SELECT @sldk = SoLuongDangKy FROM DOAN WHERE MaDoAn = '2'
-SET @sldk = @sldk + 5
-WAITFOR DELAY '00:00:05'
-
-UPDATE DOAN SET SoLuongDangKy = @sldk WHERE MaDoAn = '2'
-
-COMMIT TRAN
